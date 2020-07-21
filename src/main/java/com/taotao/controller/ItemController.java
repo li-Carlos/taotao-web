7821efbc229a84e5d97fd52efd12beab9553ea19
李柏霖 <781748856@qq.com>
2020-07-21 16:49:52 +0800

    taotao-manager-web第一天

---
+++ src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.taotao.controller;
+
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pojo.TbItem;
+
+/**
+ * 商品管理Controller
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable Long itemId) {
+        System.out.print("getItemById...run");
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+    }
+}
